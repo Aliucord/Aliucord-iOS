70dff6903e89bf212855bde3f341f9dd6830b33a
Zoey <zoey@panties.moe>
2022-01-26 17:24:07 -0500

    [Aliucord-RN] Add command to dump Discord's modules

--- Aliucord-RN/src/plugins/utilsCommands.ts
+++ Aliucord-RN/src/plugins/utilsCommands.ts
@@ -4,7 +4,7 @@ import { registerCommands } from "../api/commands";
 import { getBuild, getDevice, getSystemVersion, getVersion, reloadDiscord } from "../api/native";
 import { ApplicationCommandOptionType, ApplicationCommandInputType, ApplicationCommandType, Command } from "aliucord-api/dist/types/commands";
 
-import { connectWebsocket } from "./websocketDebug";
+import { connectWebsocket, sendMessage } from "./websocketDebug";
 import { aliucordSection } from "./pluginsManager";
 
 export function injectCommands() {
@@ -72,6 +72,56 @@ export function injectCommands() {
       const token = getToken();
       const channeld = message.channel.id;
       sendReply(channeld, `\`${token}\``);
+    }
+  }, {
+    id: "dump-command",
+    name: "dump",
+    description: "Dump Discord's modules.",
+    applicationId: aliucordSection.id,
+
+    type: ApplicationCommandType.Chat,
+    inputType: ApplicationCommandInputType.BuiltIn,
+
+    execute: function(args, message) {
+      const channeld = message.channel.id;
+      const modules = window["modules"];
+
+      for (const m of Object.keys(modules)) {
+        try {
+          // Requiring all modules so all assets get loaded into memory
+          window["__r"](Number(m));
+          const module = modules[m];
+
+          // Skip modules with no exports
+          if (Object.keys(module.publicModule.exports).length === 0) continue;
+
+          let cache = [];
+          const exports = JSON.stringify(module.publicModule.exports, (key, value) => {
+            if (typeof value === 'object' && value !== null) {
+              if (cache.includes(value)) return;
+              cache.push(value);
+            }
+            return value;
+          });
+          cache = null;
+
+
+
+          const dumpedModule = {
+            id: m,
+            exports: JSON.parse(exports)
+          };
+
+          if (module.publicModule.exports.default) {
+            dumpedModule["name"] = module.publicModule.exports.default.name;
+            dumpedModule["protoype"] = Object.getOwnPropertyNames(module.publicModule.exports.default.prototype);
+          }
+
+          sendMessage(JSON.stringify(dumpedModule, null, "\t"));
+        } catch(err) {}
+      }
+
+      sendReply(channeld, "Modules has been dumped.");
     }
   }];
 
