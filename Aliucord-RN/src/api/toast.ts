056f629f51737e0700c1af82ce2d94563d6d25b9
Zoey <zoey@panties.moe>
2022-01-30 03:05:12 -0500

    [Aliucord-RN] Fix toast api

--- Aliucord-RN/src/api/toast.ts
+++ Aliucord-RN/src/api/toast.ts
@@ -3,7 +3,7 @@ import { getModule } from "../utils/modules";
 const toastModule = getModule(m => m.default?.open && m.default?.close && !m.default?.openLazy && !m.default?.startDrag && !m.default?.init);
 
 function showToast(message) {
-  toastModule.open(message);
+  toastModule.default.open(message);
 }
 
 export {
