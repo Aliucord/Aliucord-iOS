bca666e41bf4cee2f67b2c24da7498269573b9ea
Zoey <zoey@panties.moe>
2022-03-03 22:56:56 -0500

    [tweak] Import Foundation and UIKit

--- tweak/src/Aliucord.h
+++ tweak/src/Aliucord.h
@@ -1,3 +1,6 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 #import "Commands.h"
 #import "Plugins.h"
 #import "Utils.h"
@@ -7,7 +10,7 @@
 
 // Define the URL used to download Aliucord, also disable logs in release mode
 #ifdef DEBUG
-#		define ALIUCORD_URL @"http://127.0.0.1:8080/Aliucord.js"
+#		define ALIUCORD_URL @"http://192.168.1.150:8080/Aliucord.js"
 #   define IS_DEBUG true
 #		define NSLog(fmt, ... ) NSLog((@"[Aliucord-iOS] " fmt), ##__VA_ARGS__);
 #else 
